6e85e7525af37222202d0d82041b7405ace62ad6
TamikaHayes <tamika.hayes@gmail.com>
2021-05-10 13:46:52 -0700

    Set up Express server in app.js

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
 
 'use strict';
+
+const express = require('express');
+const app = express();
+
+//const { recipes } = require('./data.json');
+
+app.get('/', (req, res) => {
+    res.send('Successful response.');
+});
+
+app.listen(3000, () => console.log('Portfolio app is listening on port 3000.'))
+
