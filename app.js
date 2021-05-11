f73c431f4238219acdc9d444747698328a8e44db
TamikaHayes <tamika.hayes@gmail.com>
2021-05-10 21:00:04 -0700

    Added routes/index.js
    --routes to index (home page, which displays all projects), about page, and individual project pages

--- app.js
+++ app.js
@@ -2,12 +2,24 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
+const { projects } = require('./data.json');
+
+
 const app = express();
 
-//const { recipes } = require('./data.json');
+//view engine setup
+app.set('view engine', 'pug');
+
+app.use(express.json());
+
+//Add static middleware
+app.use( '/static', express.static(path.join(__dirname, 'public')) );
+
 
 app.get('/', (req, res) => {
     res.send('Successful response.');
+    //console.log( { projects } );
 });
 
 app.listen(3000, () => console.log('Portfolio app is listening on port 3000.'))
---
+++ routes/index.js
@@ -0,0 +1,31 @@
+
+'use strict';
+
+const express = require('express');
+const router = express.Router();
+const { projects } = require('./data.json');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    //Pass all project data to 'index' pug template
+    res.render('index');
+});
+
+/* GET about page. */
+router.get('/about', function(req, res, next) {
+    //Pass all personal data to 'about' pug template
+    res.render('about');
+});
+
+/* GET project page. */
+router.get('/projects/:id', function(req, res, next) {
+    const projectId = req.params.id;
+    const project = projects.find( ({ id }) => id === +projectId );
+    
+    if (project) {
+      // 2. Pass the project data to the 'project' pug template
+      res.render('project');
+    } else {
+      res.sendStatus(404);
+    }
+  });
